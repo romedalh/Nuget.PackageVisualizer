f9a4e48421fe99fa1af62dddd64c3ba749b83f34
Mateusz Walo <matekw@gmail.com>
2016-04-11 23:58:30 +0200

    Repo - added initial project structure

---
+++ Nuget.DependencyVisualizer/Nuget.PackageVisualizer.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Nuget.PackageVisualizer.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
